ffd297248e71960afbbfa337859f1d5e980bd8d3
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-07-30 20:41:53 -0300

    initialize mhpasswordmanager-service-registry

---
+++ mhpasswordmanager-service-registry/src/test/java/br/com/marcoshssilva/mhpasswordmanager/service/registry/MhPasswordManagerServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.mhpasswordmanager.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhPasswordManagerServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
