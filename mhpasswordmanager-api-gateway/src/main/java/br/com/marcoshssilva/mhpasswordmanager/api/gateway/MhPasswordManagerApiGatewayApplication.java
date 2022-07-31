6620615301e91f579ba0c1098418e0a4aa9a3c28
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-07-31 01:32:18 -0300

    initialize mhpasswordmanager-api-gateway

---
+++ mhpasswordmanager-api-gateway/src/main/java/br/com/marcoshssilva/mhpasswordmanager/api/gateway/MhPasswordManagerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.mhpasswordmanager.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhPasswordManagerApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhPasswordManagerApiGatewayApplication.class, args);
+	}
+
+}
---
+++ mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/api/gateway/MhPasswordManagerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.mhpasswordmanager.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhPasswordManagerApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
