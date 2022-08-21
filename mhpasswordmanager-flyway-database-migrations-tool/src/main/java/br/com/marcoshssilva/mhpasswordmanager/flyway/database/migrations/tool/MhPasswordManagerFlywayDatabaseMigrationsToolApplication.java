7fcd5db76043db33b68109aaa54739c1054716ce
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-08-21 03:34:33 -0300

    initialize mhpasswordmanager-flyway-database-migrations-tool project.

---
+++ mhpasswordmanager-flyway-database-migrations-tool/src/main/java/br/com/marcoshssilva/mhpasswordmanager/flyway/database/migrations/tool/MhPasswordManagerFlywayDatabaseMigrationsToolApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.mhpasswordmanager.flyway.database.migrations.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhPasswordManagerFlywayDatabaseMigrationsToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhPasswordManagerFlywayDatabaseMigrationsToolApplication.class, args);
+    }
+
+}
---
+++ mhpasswordmanager-flyway-database-migrations-tool/src/test/java/br/com/marcoshssilva/mhpasswordmanager/flyway/database/migrations/tool/MhPasswordManagerFlywayDatabaseMigrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.mhpasswordmanager.flyway.database.migrations.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhPasswordManagerFlywayDatabaseMigrationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
