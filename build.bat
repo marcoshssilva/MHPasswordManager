f2ef40565c7a4023b3149da64640e8b5ac6158f5
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-09-04 04:11:23 -0300

    remove unecessary container

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-docker build -t mhpasswordmanager/flyway-migration-tool:latest mhpasswordmanager-flyway-database-migrations-tool
 docker build -t mhpasswordmanager/service-registry:latest mhpasswordmanager-service-registry
 docker build -t mhpasswordmanager/oauth2-authorization-server:latest mhpasswordmanager-oauth2-authorizationserver
 docker build -t mhpasswordmanager/api-gateway:latest mhpasswordmanager-api-gateway
@@ -6,3 +5,4 @@ docker build -t mhpasswordmanager/pwa-client:latest mhpasswordmanager-pwa-client
 docker build -t mhpasswordmanager/user-service:latest mhpasswordmanager-user-service
 
 echo 'BUILD CONTAINERS COMPLETED.'
+pause
