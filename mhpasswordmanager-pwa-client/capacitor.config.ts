2d847b79840040f6303dc590127c4954ffdaaf14
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-09-10 00:37:35 -0300

    :fix: change webDir to www

--- mhpasswordmanager-pwa-client/capacitor.config.ts
+++ mhpasswordmanager-pwa-client/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'br.com.marcoshssilva.mhpasswordmanager.pwa.client',
   appName: 'MHCofre',
-  webDir: '../mhpasswordmanager-frontend-client/src/main/resources/static',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
