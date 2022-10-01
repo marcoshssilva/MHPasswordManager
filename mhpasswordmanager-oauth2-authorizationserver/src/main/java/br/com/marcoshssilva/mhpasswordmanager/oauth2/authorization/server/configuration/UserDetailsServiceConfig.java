22f64481435cf263e4a298f6d34892baf4478b1c
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-10-01 08:43:23 -0300

    Update bean

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/UserDetailsServiceConfig.java
+++ mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/UserDetailsServiceConfig.java
@@ -2,7 +2,12 @@ package br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.confi
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
@@ -11,7 +16,18 @@ import javax.sql.DataSource;
 public class UserDetailsServiceConfig {
 
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
+    @Primary
+    public UserDetailsService jdbcUserDetailsService(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
+    }
+
+    public UserDetailsService inMemoryUserDetailsService(PasswordEncoder encoder) {
+        UserDetails userDefault = User.builder()
+                .username("user")
+                .password(encoder.encode("user"))
+                .roles("ADMIN", "USER", "MASTER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDefault);
     }
 }
