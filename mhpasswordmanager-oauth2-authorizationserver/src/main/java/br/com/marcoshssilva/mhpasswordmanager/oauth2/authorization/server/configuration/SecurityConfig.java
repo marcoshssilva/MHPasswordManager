013c68b883985a751019d665c68f4176cb861aa5
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-10-01 08:44:01 -0300

    fix: disable CSRF in H2 console path.

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/SecurityConfig.java
+++ mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/SecurityConfig.java
@@ -2,7 +2,6 @@ package br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.confi
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,11 +9,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final String[] IGNORED_CSRF_ROUTES = new String[]{"/h2/**"};
+
+    private final String[] PUBLIC_ROUTES = new String[]{};
+
     @Order(1)
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
-        return http.formLogin(Customizer.withDefaults())
-                .build();
+        // enable cors and disable csrf for specific routes
+        http.csrf().ignoringAntMatchers(IGNORED_CSRF_ROUTES);
+        // allow frame options only to sameOrigin -> fix /h2
+        http.headers().frameOptions().sameOrigin();
+        // config security routes
+        http.authorizeHttpRequests((authorize) -> authorize.antMatchers(PUBLIC_ROUTES).permitAll()
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
     }
 }
