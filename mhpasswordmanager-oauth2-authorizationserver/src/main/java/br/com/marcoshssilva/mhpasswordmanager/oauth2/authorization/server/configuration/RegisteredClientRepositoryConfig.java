27fc34c2b7f7e463924a2e8ae6cbed21f4dc8ab8
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-10-01 08:43:28 -0300

    Update bean

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/RegisteredClientRepositoryConfig.java
+++ mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/RegisteredClientRepositoryConfig.java
@@ -4,15 +4,48 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
+
+import java.time.Duration;
+import java.util.UUID;
 
 @Configuration
 public class RegisteredClientRepositoryConfig {
 
     @Bean
     @Primary
-    public RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
+    public RegisteredClientRepository jdbcRegisteredClientRepository(JdbcTemplate jdbcTemplate) {
         return new JdbcRegisteredClientRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public RegisteredClientRepository inMemoryRegisteredClientRepository() {
+        RegisteredClient clientDefault = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("master")
+                .clientSecret("masterSecret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://localhost:4200/authorize")
+                .redirectUri("http://localhost:8100/authorize")
+                .clientName("ClientMasterSystem")
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.ofDays(1))
+                        .reuseRefreshTokens(true)
+                        .build())
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(false)
+                        .requireProofKey(false)
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(clientDefault);
     }
 }
