4984d8f2fc36ae1f6dba775f40505de625d9a8b9
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2022-11-23 05:35:31 -0300

    Add @Confuguration in SecurityConfig

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/SecurityConfig.java
+++ mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/SecurityConfig.java
@@ -1,12 +1,14 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     private final String[] IGNORED_CSRF_ROUTES = new String[]{"/h2/**"};
