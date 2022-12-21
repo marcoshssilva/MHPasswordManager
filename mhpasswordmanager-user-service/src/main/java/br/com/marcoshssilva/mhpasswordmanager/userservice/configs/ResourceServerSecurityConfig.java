5f883ae9bca00a5b5b3131fcf441091d25c3f008
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2022-12-20 22:24:03 -0300

    Update ResourceServerSecurityConfig to use OAuth2ResourceServerConfigurer::jwt configuration.

--- mhpasswordmanager-user-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/userservice/configs/ResourceServerSecurityConfig.java
+++ mhpasswordmanager-user-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/userservice/configs/ResourceServerSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,11 +17,10 @@ public class ResourceServerSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated()
                 .and()
-                    .csrf().disable()
-                .oauth2ResourceServer()
-                    .jwt();
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
-        return http.build();
+        return http
+                .build();
     }
 
 }
