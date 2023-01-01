7d21f2c1ee3e81a3fc1b5240e1baf7d5089d8cbf
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2022-12-31 22:20:39 -0300

    provide customization for Login page.

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/SecurityConfig.java
+++ mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
 
         return http
                 .formLogin(Customizer.withDefaults())
+                //.formLogin(login -> login.loginPage("/login").permitAll())
                 .build();
     }
 }
