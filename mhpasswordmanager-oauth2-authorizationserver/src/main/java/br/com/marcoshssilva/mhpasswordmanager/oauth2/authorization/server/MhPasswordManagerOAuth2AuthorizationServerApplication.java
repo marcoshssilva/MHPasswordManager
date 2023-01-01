f2bced201a9c782f8a85b0e4ad8c4deb951352a1
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-01-01 02:02:44 -0300

    :new: provided support to Redis SessionStorage and Actuator.

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/MhPasswordManagerOAuth2AuthorizationServerApplication.java
+++ mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/MhPasswordManagerOAuth2AuthorizationServerApplication.java
@@ -3,8 +3,10 @@ package br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableDiscoveryClient
+@EnableRedisHttpSession
 @SpringBootApplication
 public class MhPasswordManagerOAuth2AuthorizationServerApplication {
 
