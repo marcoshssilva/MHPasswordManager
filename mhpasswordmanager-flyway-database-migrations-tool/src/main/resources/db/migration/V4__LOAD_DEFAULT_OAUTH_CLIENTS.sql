f661889e617615cc166e86b28da3cf7e8cc107c4
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-01-22 22:12:27 -0300

    fix: remove enabled redirect_uri to electron

--- mhpasswordmanager-flyway-database-migrations-tool/src/main/resources/db/migration/V4__LOAD_DEFAULT_OAUTH_CLIENTS.sql
+++ mhpasswordmanager-flyway-database-migrations-tool/src/main/resources/db/migration/V4__LOAD_DEFAULT_OAUTH_CLIENTS.sql
@@ -5,7 +5,7 @@ INSERT INTO db_users.oauth2_registered_client (id, client_id, client_id_issued_a
 VALUES ('0fbef37c-443c-4084-aa68-83914e28f182', 'MHPasswordManager', '2023-01-17 05:49:34.776',
         '$2a$10$6dTcMLYP4dYKuc5qfxvomu3ZzXYTqjx.hkEEgBadlJNTlqSxwf5H.', NULL, 'Registered client for PWA Client',
         'client_secret_basic', 'refresh_token,authorization_code',
-        'http://127.0.0.1:4200/authorize,http://127.0.0.1:8100/authorize,capacitor-electron://-/authorize,https://oidcdebugger.com/debug,https://oauth.pstmn.io/v1/callback',
+        'http://127.0.0.1:4200/authorize,http://127.0.0.1:8100/authorize,https://oidcdebugger.com/debug,https://oauth.pstmn.io/v1/callback',
         'user:canSelfRead,user:canSelfDelete,user:canSelfWrite',
         '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":true}',
         '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",900.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",10800.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000]}'),
