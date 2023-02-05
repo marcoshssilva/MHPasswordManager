46d537302b4fc0250ca42b9508ccb9fd7ce19f85
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-02-05 19:34:49 -0300

    fix: docker - update name of docker project compose

--- docker-compose-files/run.sh
+++ docker-compose-files/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-set COMPOSE_CONVERT_WINDOWS_PATHS=1
-set COMPOSE_PROJECT_NAME=MHPASSWORDMANAGER
+export COMPOSE_CONVERT_WINDOWS_PATHS="1"
+export COMPOSE_PROJECT_NAME="MHPASSWORDMANAGER"
 docker-compose -f "docker-compose.yml" up -d eureka-service-discovery oauth2-authorization-server api-gateway user-service password-service
