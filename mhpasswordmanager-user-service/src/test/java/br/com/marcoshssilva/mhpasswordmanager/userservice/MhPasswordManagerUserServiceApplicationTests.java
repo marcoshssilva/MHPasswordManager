040d72179721b551a2b8e270ed184cf2e26b5b2c
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-02-25 04:45:25 -0300

    fix(tests): config UnitTests for UserServiceApi

--- mhpasswordmanager-user-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/userservice/MhPasswordManagerUserServiceApplicationTests.java
+++ mhpasswordmanager-user-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/userservice/MhPasswordManagerUserServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.marcoshssilva.mhpasswordmanager.userservice;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MhPasswordManagerUserServiceApplicationTests {
+	private final Logger LOG = LoggerFactory.getLogger(MhPasswordManagerUserServiceApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		LOG.info("Application started with SUCCESS.");
 	}
 
 }
