ad5a7bd804a702cc5366b87888492900020dbbc3
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-02-25 03:42:23 -0300

    refactor: include Log in MhPasswordManagerPasswordServiceApplicationTests

--- mhpasswordmanager-password-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/MhPasswordManagerPasswordServiceApplicationTests.java
+++ mhpasswordmanager-password-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/MhPasswordManagerPasswordServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MhPasswordManagerPasswordServiceApplicationTests {
+	private final Logger LOG = LoggerFactory.getLogger(MhPasswordManagerPasswordServiceApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		LOG.info("Project started with Success!");
 	}
 
 }
