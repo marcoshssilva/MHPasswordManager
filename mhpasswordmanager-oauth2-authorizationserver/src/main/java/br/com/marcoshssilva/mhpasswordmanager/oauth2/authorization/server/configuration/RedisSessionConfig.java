b72c9ff3c9bf46842c665b3c1bd2b6579e7a01b8
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-03-11 08:15:55 -0300

    fix(database): update RedisSession

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/RedisSessionConfig.java
+++ mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/configuration/RedisSessionConfig.java
@@ -1,8 +1,10 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@Profile("!test")
+@Configuration
+@Profile("redis & !test")
 @EnableRedisHttpSession
 public class RedisSessionConfig { }
