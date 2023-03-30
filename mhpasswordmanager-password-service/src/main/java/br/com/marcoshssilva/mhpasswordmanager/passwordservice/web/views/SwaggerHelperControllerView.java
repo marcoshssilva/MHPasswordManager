b105af0100c20075f511e79dc0404929cbdd480c
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-03-30 06:57:19 -0300

    feature(flyway-support): MHPasswordManager-UserService - update Swagger, include swagger-redirect-uri.

---
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/web/views/SwaggerHelperControllerView.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.mhpasswordmanager.passwordservice.web.views;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerHelperControllerView {
+    @GetMapping("/swagger-ui/redirect")
+    public String oAuth2SwaggerRedirectUri(Model model) {
+        return "oauth2-redirect";
+    }
+}
