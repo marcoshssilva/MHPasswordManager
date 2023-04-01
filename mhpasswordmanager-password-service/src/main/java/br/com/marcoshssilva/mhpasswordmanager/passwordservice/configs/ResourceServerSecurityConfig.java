3f6b0af889ccb69c2de76708f0a87ecdf15cb7c4
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-04-01 14:50:59 -0300

    feature(flyway-support): MHPasswordManager-Password-Service - review swagger-ui.

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/configs/ResourceServerSecurityConfig.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/configs/ResourceServerSecurityConfig.java
@@ -1,11 +1,13 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.configs;
 
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.configs.security.CustomJwtGrantedAuthoritiesConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,8 +23,18 @@ public class ResourceServerSecurityConfig {
                     // any other requests authenticated
                     .anyRequest().authenticated()
                 .and()
-                    .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                .build();
+                    .oauth2ResourceServer()
+                        .jwt()
+                        // set custom converter to includes all authorities from Jwt token
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        .and()
+                .and().build();
     }
 
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new CustomJwtGrantedAuthoritiesConverter());
+        return converter;
+    }
 }
