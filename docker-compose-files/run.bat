03158510c655dc08002eabbd960a9af181584093
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-04-01 14:58:33 -0300

    feature(docker): Update docker-compose.

--- docker-compose-files/run.bat
+++ docker-compose-files/run.bat
@@ -1,4 +1,4 @@
 set COMPOSE_CONVERT_WINDOWS_PATHS=1
 set COMPOSE_PROJECT_NAME=MHPASSWORDMANAGER
-docker-compose -f "docker-compose.yml" up -d eureka-service-discovery oauth2-authorization-server api-gateway user-service password-service
+docker-compose -f "docker-compose.yml" up -d postgres-db eureka-service-discovery oauth2-authorization-server api-gateway user-service password-service redis-store
 pause
