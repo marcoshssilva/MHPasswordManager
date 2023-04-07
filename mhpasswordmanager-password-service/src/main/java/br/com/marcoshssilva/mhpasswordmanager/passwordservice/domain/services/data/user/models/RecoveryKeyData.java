beba600d7a17e9bd2f0192a152df11c80e49cfbc
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-04-07 17:49:03 -0300

    feature(sonar): MHPasswordManager-Password-Service - fix sonar: Override equals, hashCode and toString to consider array's content in the method

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/user/models/RecoveryKeyData.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/user/models/RecoveryKeyData.java
@@ -1,9 +1,25 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.user.models;
 
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Objects;
 
 public record RecoveryKeyData(String key, byte[] encryptedData) {
     public String getEncryptedDataAsBase64() {
         return Base64.getEncoder().encodeToString(this.encryptedData);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RecoveryKeyData that)) return false;
+        return Objects.equals(key, that.key) && Arrays.equals(encryptedData, that.encryptedData);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(key);
+        result = 31 * result + Arrays.hashCode(encryptedData);
+        return result;
     }
 }
