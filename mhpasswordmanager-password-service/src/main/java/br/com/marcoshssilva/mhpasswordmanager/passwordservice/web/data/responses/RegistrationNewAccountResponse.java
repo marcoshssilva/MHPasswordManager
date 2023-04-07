cdff1e8dbce084f2dacb88b028b42eedbaec7e4b
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-04-07 19:21:49 -0300

    feature(sonar): MHPasswordManager-Password-Service - add getters, settes and toString to RegistrationNewAccountResponse.java

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/web/data/responses/RegistrationNewAccountResponse.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/web/data/responses/RegistrationNewAccountResponse.java
@@ -1,8 +1,13 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.web.data.responses;
 
+import lombok.Data;
+import lombok.ToString;
+
 import java.io.Serializable;
 import java.util.Set;
 
+@Data
+@ToString
 public class RegistrationNewAccountResponse implements Serializable {
     private static final long serialVersionUID = 1L;
 
