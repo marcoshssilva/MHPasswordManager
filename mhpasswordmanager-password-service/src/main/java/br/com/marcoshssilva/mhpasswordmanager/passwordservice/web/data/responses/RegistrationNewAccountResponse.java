e228db4fa0b103a82e772af3d906b2e5a800f81a
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-04-09 00:42:29 -0300

    feature(refactor-code): MHPasswordManager-Password-Service - refactor change lombok.Data to lombok.Getter and lombok.Setter

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/web/data/responses/RegistrationNewAccountResponse.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/web/data/responses/RegistrationNewAccountResponse.java
@@ -1,15 +1,17 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.web.data.responses;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.Set;
 
-@Data
 @ToString
+@Getter
+@Setter
 public class RegistrationNewAccountResponse implements Serializable {
-    private static final long serialVersionUID = 1L;
+    public static final long serialVersionUID = 1L;
 
     private String email;
     private Set<String> recoveryCodes;
