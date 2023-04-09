15dfbec0f9ae69c9528d42b95ee8e6634d977895
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-04-08 22:25:44 -0300

    feature(refactor-code): MHPasswordManager-Password-Service - refactor unit test in UserPasswordStoredValueTests

--- mhpasswordmanager-password-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/entities/UserPasswordStoredValueTests.java
+++ mhpasswordmanager-password-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/entities/UserPasswordStoredValueTests.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserPasswordStoredValueTests {
     private UserPasswordStoredValue userPasswordStoredValue;
@@ -35,23 +34,79 @@ class UserPasswordStoredValueTests {
         assertEquals(now, userPasswordStoredValue.getLastUpdate());
     }
 
-    @DisplayName("Test hashCode and equals methods")
+    @DisplayName("Test equals method")
     @Test
     void testHashCodeAndEquals() {
-        UserPasswordStoredValue obj1 = new UserPasswordStoredValue();
-        obj1.setId(1L);
+        UserPasswordKey key = new UserPasswordKey();
+        key.setId(1L);
 
-        UserPasswordStoredValue obj2 = new UserPasswordStoredValue();
-        obj2.setId(1L);
+        UserPasswordStoredValue value1 = UserPasswordStoredValue.builder()
+                .id(1L)
+                .keyId(key)
+                .data("data1")
+                .createdAt(new Date())
+                .lastUpdate(new Date())
+                .build();
 
-        UserPasswordStoredValue obj3 = new UserPasswordStoredValue();
-        obj3.setId(2L);
+        UserPasswordStoredValue value2 = UserPasswordStoredValue.builder()
+                .id(1L)
+                .keyId(key)
+                .data("data2")
+                .createdAt(new Date())
+                .lastUpdate(new Date())
+                .build();
 
-        assertEquals(obj1.hashCode(), obj2.hashCode());
-        assertNotEquals(obj1.hashCode(), obj3.hashCode());
+        UserPasswordStoredValue value3 = UserPasswordStoredValue.builder()
+                .id(2L)
+                .keyId(key)
+                .data("data1")
+                .createdAt(new Date())
+                .lastUpdate(new Date())
+                .build();
 
-        assertEquals(obj1, obj2);
-        assertNotEquals(obj1, obj3);
-        assertNotEquals(obj2, obj3);
+        // Reflexive
+        assertTrue(value1.equals(value1));
+
+        // Symmetric
+        assertTrue(value1.equals(value2));
+        assertTrue(value2.equals(value1));
+
+        // Transitive
+        assertFalse(value2.equals(value3));
+        assertFalse(value1.equals(value3));
+
+        // Not equal
+        assertFalse(value1.equals(null));
+        assertFalse(value1.equals("value1"));
+        assertFalse(value1.equals(value2.hashCode()));
+    }
+
+    @DisplayName("Test hashCode method")
+    @Test
+    void testHashCode() {
+        UserPasswordKey key = new UserPasswordKey();
+        key.setId(1L);
+
+        UserPasswordStoredValue value1 = UserPasswordStoredValue.builder()
+                .id(1L)
+                .keyId(key)
+                .data("data1")
+                .createdAt(new Date())
+                .lastUpdate(new Date())
+                .build();
+
+        UserPasswordStoredValue value2 = UserPasswordStoredValue.builder()
+                .id(1L)
+                .keyId(key)
+                .data("data2")
+                .createdAt(new Date())
+                .lastUpdate(new Date())
+                .build();
+
+        // Consistent
+        assertEquals(value1.hashCode(), value1.hashCode());
+
+        // Equals objects have the same hashCode
+        assertEquals(value1.hashCode(), value2.hashCode());
     }
 }
