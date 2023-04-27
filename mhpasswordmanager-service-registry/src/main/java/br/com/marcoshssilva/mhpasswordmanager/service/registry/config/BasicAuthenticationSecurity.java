d02a5fcc84ccc69ed2d45bd20be9fcce55b94ee7
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-04-26 23:11:25 -0300

    feature(MHPasswordManager-Service-Registry): Enable WebSecurity with config for Basic Authentication in project.

---
+++ mhpasswordmanager-service-registry/src/main/java/br/com/marcoshssilva/mhpasswordmanager/service/registry/config/BasicAuthenticationSecurity.java
@@ -0,0 +1,21 @@
+package br.com.marcoshssilva.mhpasswordmanager.service.registry.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class BasicAuthenticationSecurity {
+    @Bean
+    SecurityFilterChain basicAuthenticationSecurityFilterChainConfigurer(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic().and()
+                .build();
+    }
+}
--- mhpasswordmanager-service-registry/src/test/java/br/com/marcoshssilva/mhpasswordmanager/service/registry/MhPasswordManagerServiceRegistryApplicationTests.java
+++ mhpasswordmanager-service-registry/src/test/java/br/com/marcoshssilva/mhpasswordmanager/service/registry/MhPasswordManagerServiceRegistryApplicationTests.java
@@ -1,13 +1,44 @@
 package br.com.marcoshssilva.mhpasswordmanager.service.registry;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class MhPasswordManagerServiceRegistryApplicationTests {
+	private final Logger LOG = LoggerFactory.getLogger(MhPasswordManagerServiceRegistryApplicationTests.class);
 
+	@DisplayName("Should initialize project with success")
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> {
+			LOG.info("Project started with success!");
+		});
 	}
 
+	@DisplayName("Should call main but using a ")
+	@Test
+	void shouldRunApplicationWithMock() {
+		String[] args = new String[]{"1", "2", "3"};
+		try (MockedStatic<SpringApplication> mocked = mockStatic(SpringApplication.class)) {
+			mocked.when(() -> {
+						SpringApplication.run(MhPasswordManagerServiceRegistryApplication.class, args);
+					})
+					.thenReturn(Mockito.mock(ConfigurableApplicationContext.class));
+
+			MhPasswordManagerServiceRegistryApplication.main(args);
+			mocked.verify(() -> {
+				SpringApplication.run(MhPasswordManagerServiceRegistryApplication.class, args);
+			});
+		}
+	}
 }
