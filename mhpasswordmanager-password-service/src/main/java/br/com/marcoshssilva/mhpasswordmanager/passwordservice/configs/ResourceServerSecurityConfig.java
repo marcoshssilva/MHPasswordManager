123f884947ea3ac2268e287154a7641d42e127b4
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-05-02 00:17:27 -0300

    refactor: removed CustomJwtGrantedAuthoritiesConverter, is not necessary

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/configs/ResourceServerSecurityConfig.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/configs/ResourceServerSecurityConfig.java
@@ -1,12 +1,10 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.configs;
 
-import br.com.marcoshssilva.mhpasswordmanager.passwordservice.configs.security.CustomJwtGrantedAuthoritiesConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,23 +15,14 @@ public class ResourceServerSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeRequests()
-                    // enable Swagger MVC public
-                    .antMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/actuator/**").permitAll()
-                    // any other requests authenticated
-                    .anyRequest().authenticated()
+                // enable Swagger MVC public
+                .antMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/actuator/**").permitAll()
+                // any other requests authenticated
+                .anyRequest().authenticated()
                 .and()
-                    .oauth2ResourceServer()
-                        .jwt()
-                        // set custom converter to includes all authorities from Jwt token
-                        .jwtAuthenticationConverter(jwtAuthenticationConverter())
-                        .and()
-                .and().build();
+                .oauth2ResourceServer()
+                .jwt()
+                .and().and().build();
     }
 
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(new CustomJwtGrantedAuthoritiesConverter());
-        return converter;
-    }
 }
