feba7006281a8b3828c362fa374968b40fe53512
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-05-02 00:21:12 -0300

    Create methods to search data

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/repositories/UserPasswordKeyRepository.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/repositories/UserPasswordKeyRepository.java
@@ -1,9 +1,19 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.repositories;
 
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.entities.UserPasswordKey;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserPasswordKeyRepository extends JpaRepository<UserPasswordKey, Long> {
+    @Query(value = "SELECT obj FROM UserPasswordKey obj WHERE obj.id = ?2 AND obj.userRegistration.email = ?1")
+    Optional<UserPasswordKey> findByUserRegistrationEmailAndId(String email, Long id);
+
+    @Query(value = "SELECT obj FROM UserPasswordKey obj WHERE obj.userRegistration.email = ?1")
+    Page<UserPasswordKey> findAllByUserRegistrationEmail(String email, Pageable pageable);
 }
--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/repositories/UserPasswordStoredValueRepository.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/repositories/UserPasswordStoredValueRepository.java
@@ -1,9 +1,13 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.repositories;
 
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.entities.UserPasswordKey;
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.entities.UserPasswordStoredValue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface UserPasswordStoredValueRepository extends JpaRepository<UserPasswordStoredValue, Long> {
+    Set<UserPasswordStoredValue> findAllByKeyId(UserPasswordKey id);
 }
