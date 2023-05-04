a65f93c68775064606c6b93141e447499cc91ef9
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-05-04 02:07:17 -0300

    feature(passwords): Create endpoint to save key

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/web/controllers/ManageKeysController.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/web/controllers/ManageKeysController.java
@@ -1,6 +1,13 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.web.controllers;
 
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.keys.KeyRegistrationErrorException;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.keys.UserKeysService;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.keys.converters.KeyEncodedErrorConverterException;
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.keys.models.AbstractKeyPayloadDecodedDto;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.keys.models.KeyPayloadEncodedDto;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.user.UserRegistrationNotFoundException;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.user.UserRegistrationService;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.user.models.UserRegisteredModel;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,6 +23,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+
 @RestController
 @RequestMapping("/keys")
 @RequiredArgsConstructor
@@ -23,6 +32,8 @@ import org.springframework.web.bind.annotation.*;
 @SecurityRequirement(name = "OAuth2 Authorization Code Flow")
 @SecurityRequirement(name = "Bearer Authorization")
 public class ManageKeysController {
+    private final UserRegistrationService userRegistrationService;
+    private final UserKeysService userKeysService;
 
     @GetMapping("{uuid}/get/all")
     public ResponseEntity<Void> getAllKeys(@AuthenticationPrincipal Jwt token, @PathVariable("uuid") String uuid, @ParameterObject @PageableDefault(page = 0, size = 500, sort = {"createdAt DESC"}) Pageable pageable) {
@@ -35,8 +46,19 @@ public class ManageKeysController {
     }
 
     @PostMapping("/{uuid}/post/new")
-    public ResponseEntity<Void> saveKey(@AuthenticationPrincipal Jwt token, @RequestBody AbstractKeyPayloadDecodedDto payload, @PathVariable("uuid") String uuid) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    public ResponseEntity<KeyPayloadEncodedDto> saveKey(@AuthenticationPrincipal Jwt token, @RequestBody AbstractKeyPayloadDecodedDto payload, @PathVariable("uuid") String uuid) throws UserRegistrationNotFoundException, KeyEncodedErrorConverterException, KeyRegistrationErrorException {
+
+        UserRegisteredModel userRegistration = userRegistrationService.getUserRegistration(token.getSubject());
+        if (Boolean.FALSE.equals(uuid.equals(userRegistration.getUuid()))) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        KeyPayloadEncodedDto keyPayloadEncodedDto = userKeysService.transformAsKeyPayloadEncodedDto(payload, userRegistration.getPublicKey());
+        keyPayloadEncodedDto.setId(null);
+        Arrays.stream(keyPayloadEncodedDto.getEncodedKeys()).forEach(item -> { item.setId(null); });
+
+        KeyPayloadEncodedDto saved = userKeysService.saveKeyPayloadEncodedDto(keyPayloadEncodedDto);
+        return ResponseEntity.ok(saved);
     }
 
     @PutMapping("/{uuid}/put/{id}")
