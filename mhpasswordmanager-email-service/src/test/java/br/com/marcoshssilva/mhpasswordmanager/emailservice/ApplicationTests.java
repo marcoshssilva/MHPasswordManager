4043161094cb984da9fb5ded5eaf444b8f6cabf6
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-05-17 20:06:19 -0300

    feature(email): Fix tests - disable RabbitMq on ContextLoad

--- mhpasswordmanager-email-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/emailservice/ApplicationTests.java
+++ mhpasswordmanager-email-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/emailservice/ApplicationTests.java
@@ -1,11 +1,15 @@
 package br.com.marcoshssilva.mhpasswordmanager.emailservice;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {RabbitAutoConfiguration.class})
 class ApplicationTests {
 
 	@Test
