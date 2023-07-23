556249ff1c310918b5ab7ff276dadf4c2d4d1348
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-07-23 14:31:17 -0300

    feature(config): Update all environtments

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/domain/service/impl/JwkKeyServiceImpl.java
+++ mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/domain/service/impl/JwkKeyServiceImpl.java
@@ -1,6 +1,5 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.domain.service.impl;
 
-import br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.configuration.AuthorizationConfigProperties;
 import br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.domain.exceptions.JwkLoaderFailException;
 import br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.domain.service.JwkKeyService;
 
@@ -9,9 +8,10 @@ import com.nimbusds.jose.jwk.RSAKey;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -25,12 +25,12 @@ import java.util.Base64;
 @Slf4j
 public class JwkKeyServiceImpl implements JwkKeyService {
     private static final String ALGORITHM = "RSA";
-    private final AuthorizationConfigProperties authorizationConfigProperties;
+    private static final String UUID = "0107f47a-2263-421c-81bb-10210c9c2e6d";
 
     @Override
     public PublicKey getPublicKey() throws JwkLoaderFailException {
-        try {
-            String base64PublicKey = authorizationConfigProperties.getJwkPublicKey()
+        try (InputStream resource = new ClassPathResource("/public-key.pem").getInputStream()) {
+            String base64PublicKey = new String(resource.readAllBytes())
                     .replace("\n", "")
                     .replace("-----BEGIN PUBLIC KEY-----", "")
                     .replace("-----END PUBLIC KEY-----", "");
@@ -39,6 +39,7 @@ public class JwkKeyServiceImpl implements JwkKeyService {
             log.info(base64PublicKey);
             X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey));
             return kf.generatePublic(x509EncodedKeySpec);
+
         } catch (Exception e) {
             throw new JwkLoaderFailException(e.getMessage(), e);
         }
@@ -47,8 +48,8 @@ public class JwkKeyServiceImpl implements JwkKeyService {
 
     @Override
     public PrivateKey getPrivateKey() throws JwkLoaderFailException {
-        try {
-            String base64PrivateKey = authorizationConfigProperties.getJwkPrivateKey()
+        try (InputStream resource = new ClassPathResource("/private-key.pem").getInputStream()) {
+            String base64PrivateKey = new String(resource.readAllBytes())
                     .replace("\n", "")
                     .replace("-----BEGIN PRIVATE KEY-----", "")
                     .replace("-----END PRIVATE KEY-----", "");
@@ -56,6 +57,7 @@ public class JwkKeyServiceImpl implements JwkKeyService {
             KeyFactory kf = KeyFactory.getInstance(ALGORITHM);
             PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(base64PrivateKey));
             return kf.generatePrivate(pkcs8EncodedKeySpec);
+
         } catch (Exception e) {
             throw new JwkLoaderFailException(e.getMessage(), e);
         }
@@ -64,9 +66,6 @@ public class JwkKeyServiceImpl implements JwkKeyService {
 
     @Override
     public RSAKey getRSAKey() throws JwkLoaderFailException {
-        return new RSAKey.Builder((RSAPublicKey) getPublicKey())
-                .privateKey(getPrivateKey())
-                .keyID(authorizationConfigProperties.getJwkUuid())
-                .build();
+        return new RSAKey.Builder((RSAPublicKey) getPublicKey()).privateKey(getPrivateKey()).keyID(UUID).build();
     }
 }
