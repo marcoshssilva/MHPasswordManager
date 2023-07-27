22599ce08a00019824d677220dccd7e79ae35356
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2023-07-27 06:01:52 -0300

    feature(docker): Fix docker build steps

--- docker/jenkins/build-x86-64.sh
+++ docker/jenkins/build-x86-64.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-docker build -t mhpasswordmanager/api-gateway:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-api-gateway
+docker build --build-arg="SERVICE_PORT=12050" -t mhpasswordmanager/api-gateway:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-api-gateway
 
-docker build -t mhpasswordmanager/config-services:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-config-services
+docker build --build-arg="SERVICE_PORT=8888" -t mhpasswordmanager/config-services:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-config-services
 
-docker build -t mhpasswordmanager/oauth2-server:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-oauth2-authorizationserver
+docker build --build-arg="SERVICE_PORT=12010" -t mhpasswordmanager/oauth2-server:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-oauth2-authorizationserver
 
-docker build -t mhpasswordmanager/eureka-server:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-service-registry
+docker build --build-arg="SERVICE_PORT=8761" -t mhpasswordmanager/eureka-server:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-service-registry
 
-docker build -t mhpasswordmanager/user-service:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-user-service
+docker build --build-arg="SERVICE_PORT=12011" -t mhpasswordmanager/user-service:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-user-service
 
-docker build -t mhpasswordmanager/password-service:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-password-service
+docker build --build-arg="SERVICE_PORT=12012" -t mhpasswordmanager/password-service:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-password-service
 
-docker build -t mhpasswordmanager/email-service:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-email-service
+docker build --build-arg="SERVICE_PORT=12013" -t mhpasswordmanager/email-service:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-email-service
 
-docker build -t mhpasswordmanager/file-service:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-file-service
+docker build --build-arg="SERVICE_PORT=12014" -t mhpasswordmanager/file-service:"$1" -f Dockerfile-x86-64 ../../mhpasswordmanager-file-service
