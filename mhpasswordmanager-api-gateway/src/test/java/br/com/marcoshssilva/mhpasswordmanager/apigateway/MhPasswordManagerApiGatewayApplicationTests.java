2d10be6127205f576420e90c810b3b1d84611c7b
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-12-04 01:18:34 -0300

    Update folder api-gateway

--- mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/apigateway/MhPasswordManagerApiGatewayApplicationTests.java
+++ mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/apigateway/MhPasswordManagerApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.marcoshssilva.mhpasswordmanager.api.gateway;
+package br.com.marcoshssilva.mhpasswordmanager.apigateway;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
