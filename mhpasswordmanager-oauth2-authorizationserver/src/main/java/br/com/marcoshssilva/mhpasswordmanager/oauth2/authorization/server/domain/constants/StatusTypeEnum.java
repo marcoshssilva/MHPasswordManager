a9622a571fc1966ec19146533d11c11668e9aea4
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-12-04 01:24:11 -0300

    Delete files

--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/domain/constants/StatusTypeEnum.java
+++
@@ -1,6 +0,0 @@
-package br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.domain.constants;
-
-public enum StatusTypeEnum {
-    ERROR,
-    SUCCESS
-}
--- mhpasswordmanager-oauth2-authorizationserver/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2/authorization/server/domain/service/SendEmailService.java
+++
@@ -1,8 +0,0 @@
-package br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.domain.service;
-
-import br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.domain.exceptions.FailSendEmailException;
-import br.com.marcoshssilva.mhpasswordmanager.oauth2.authorization.server.domain.models.RegisteredUserKeyVerificationMailMessage;
-
-public interface SendEmailService {
-    void sendEmailRecoveryPassword(RegisteredUserKeyVerificationMailMessage message) throws FailSendEmailException;
-}
