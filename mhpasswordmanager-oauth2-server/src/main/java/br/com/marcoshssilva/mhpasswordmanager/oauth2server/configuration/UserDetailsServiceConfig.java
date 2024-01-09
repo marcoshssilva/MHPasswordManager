323c338726171b2c647684ba5e882255f12fd5e0
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-01-08 22:34:03 -0300

    feature: sonarqube-coverage - fix bug 'Revoke and change this password, as it is compromised.'

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/configuration/UserDetailsServiceConfig.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/configuration/UserDetailsServiceConfig.java
@@ -1,67 +1,37 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2server.configuration;
 
-import br.com.marcoshssilva.mhpasswordmanager.oauth2server.domain.constants.UserRolesEnum;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
 
+@Slf4j
 @Configuration
 public class UserDetailsServiceConfig {
-    private static final Logger LOG = LoggerFactory.getLogger(UserDetailsServiceConfig.class);
-
     @Bean
     @Profile("in-memory-users")
-    public UserDetailsManager inMemoryUserDetailsService(PasswordEncoder encoder) {
-        final String logMessage = "UserDetailsService using InMemoryUserDetailsManager with default user: {} and password: {} - encrypted: {} - ROLES: {}";
-
-        // user administrator
-        final String user1 = "admin@mhpasswordmanager.com";
-        final String pass1 = "P@ssword123";
-
-        UserDetails userDefault = User.builder()
-                .username(user1)
-                .password(encoder.encode(pass1))
-                .roles(UserRolesEnum.ADMIN.name(), UserRolesEnum.MASTER.name(), UserRolesEnum.USER.name())
-                .build();
-
-        LOG.info(logMessage, user1, pass1, userDefault.getPassword(), userDefault.getAuthorities());
-
-        // user anonymous
-        final String user2 = "anonymous@mhpasswordmanager.com";
-        final String pass2 = "P@ssword123";
-
-        UserDetails userAnonymous = User.builder()
-                .username(user2)
-                .password(encoder.encode(pass2))
-                .roles(UserRolesEnum.USER.name())
-                .build();
-
-        LOG.info(logMessage, user2, pass2, userAnonymous.getPassword(), userAnonymous.getAuthorities());
-
-        // save data
-        return new InMemoryUserDetailsManager(userDefault, userAnonymous);
+    public UserDetailsManager inMemoryUserDetailsService() {
+        log.info("UserDetailsManager using InMemoryUserDetailsManager with default users and No Database");
+        return new InMemoryUserDetailsManager();
     }
 
     @Bean
     @Profile("!embedded-database & !in-memory-users")
     public UserDetailsManager jdbcUserDetailsManager(@Qualifier("dataSourceDbUsers") DataSource dataSource) {
+        log.info("UserDetailsManager using JdbcUserDetailsManager with users-db datasource");
         return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
     @Profile("embedded-database")
     public UserDetailsManager jdbcUserDetailsManagerForEmbedded(@Qualifier("embeddedDatabase") DataSource dataSource) {
+        log.info("UserDetailsManager using JdbcUserDetailsManager with embedded-database");
         return new JdbcUserDetailsManager(dataSource);
     }
 }
