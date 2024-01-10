ad0eed6dab9449e6a8d1df921168a53fb758394a
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-01-09 23:05:26 -0300

    feature: sonarqube-coverage - fix security hotspot "Disabling CSRF protections is security-sensitive"

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/configuration/SecurityConfig.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/configuration/SecurityConfig.java
@@ -16,12 +16,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-    // injected
     private final AuthorizationConfigProperties authorizationConfigProperties;
 
-    private final String[] IGNORED_CSRF_ROUTES = new String[]{"/h2/**"};
-
-    private final String[] PUBLIC_ROUTES = new String[]{"/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/content/**", "/actuator/**"};
+    private final String[] PUBLIC_ROUTES = new String[]{"/h2/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/content/**", "/actuator/**"};
 
     private final String[] GET_METHOD_ONLY_PUBLIC = new String[]{"/verify/**"};
 
@@ -31,15 +28,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         // csrf for specific routes
-        http.csrf().ignoringAntMatchers(IGNORED_CSRF_ROUTES).csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         // allow frame options only to sameOrigin -> fix /h2
         http.headers().frameOptions().sameOrigin();
         // config security routes
         http.authorizeHttpRequests((authorize) -> authorize
                 // register public routes
                 .antMatchers(PUBLIC_ROUTES).permitAll()
-                // register available path -> /h2 <- only to ROLE_ADMIN and ROLE_MASTER
-                .antMatchers("/h2/**").hasAnyRole(UserRolesEnum.ADMIN.name(), UserRolesEnum.MASTER.name())
                 // register as public access only when GET method
                 .antMatchers(HttpMethod.GET, GET_METHOD_ONLY_PUBLIC).permitAll()
                 // register as public access only when POST method
