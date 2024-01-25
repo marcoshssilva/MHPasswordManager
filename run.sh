01f47de7a586239481cd4f8c61099df6971259da
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-01-25 04:40:59 -0300

    Change docker compose to use profiles

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export COMPOSE_CONVERT_WINDOWS_PATHS="1"
 export COMPOSE_PROJECT_NAME="MHPASSWORDMANAGER"
-docker-compose -f "docker-compose.yml" up -d
+docker-compose -f "docker-compose.yml" up -d --profile all
