a8150bebf4704ce0b902c778b365f6f7ba51396e
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-01-31 20:51:24 -0300

    feat(password-service): wip UserBucketService

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/buckets/UserBucketService.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/buckets/UserBucketService.java
@@ -1,8 +1,14 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets;
 
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.exceptions.BucketCannotBeCreatedException;
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.exceptions.BucketNotFoundException;
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.models.BucketDataModel;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.models.BucketNewDataModel;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.common.IResultData;
 
 public interface UserBucketService {
-    BucketDataModel getBucketByUuid(String bucketUuid) throws BucketNotFoundException;
+    IResultData<BucketDataModel> getBucketByUuid(String bucketUuid) throws BucketNotFoundException;
+    IResultData<BucketDataModel> createBucket(BucketNewDataModel bucketNewDataModel) throws BucketCannotBeCreatedException;
+    IResultData<BucketDataModel> updateBucket(String bucketUuid, BucketNewDataModel bucketNewDataModel) throws BucketNotFoundException;
+    IResultData<Boolean> deleteBucketByUuid(String bucketUuid) throws BucketNotFoundException;
 }
---
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/buckets/models/BucketNewDataModel.java
@@ -0,0 +1,28 @@
+package br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.models;
+
+import java.util.Objects;
+
+@lombok.ToString
+@lombok.NoArgsConstructor
+@lombok.AllArgsConstructor
+@lombok.Builder
+@lombok.Getter
+@lombok.Setter
+public class BucketNewDataModel {
+    private String bucketName;
+    private String bucketDescription;
+    private String userOwner;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BucketNewDataModel that = (BucketNewDataModel) o;
+        return Objects.equals(bucketName, that.bucketName) && Objects.equals(bucketDescription, that.bucketDescription) && Objects.equals(userOwner, that.userOwner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bucketName, bucketDescription, userOwner);
+    }
+}
