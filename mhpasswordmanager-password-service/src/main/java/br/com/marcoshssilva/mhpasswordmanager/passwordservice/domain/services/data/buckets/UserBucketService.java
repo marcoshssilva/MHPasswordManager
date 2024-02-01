dd55cb7ace34471062628f5a27c7ab82420c4085
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-01-31 21:38:32 -0300

    feat(password-service): update UserBucketService to use BucketUpdateDataModel when call updateBucket() method

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/buckets/UserBucketService.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/buckets/UserBucketService.java
@@ -4,11 +4,12 @@ import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.da
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.exceptions.BucketNotFoundException;
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.models.BucketDataModel;
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.models.BucketNewDataModel;
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.buckets.models.BucketUpdateDataModel;
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.common.IResultData;
 
 public interface UserBucketService {
     IResultData<BucketDataModel> getBucketByUuid(String bucketUuid) throws BucketNotFoundException;
     IResultData<BucketDataModel> createBucket(BucketNewDataModel bucketNewDataModel) throws BucketCannotBeCreatedException;
-    IResultData<BucketDataModel> updateBucket(String bucketUuid, BucketNewDataModel bucketNewDataModel) throws BucketNotFoundException;
+    IResultData<BucketDataModel> updateBucket(String bucketUuid, BucketUpdateDataModel bucketUpdateDataModel) throws BucketNotFoundException;
     IResultData<Boolean> deleteBucketByUuid(String bucketUuid) throws BucketNotFoundException;
 }
