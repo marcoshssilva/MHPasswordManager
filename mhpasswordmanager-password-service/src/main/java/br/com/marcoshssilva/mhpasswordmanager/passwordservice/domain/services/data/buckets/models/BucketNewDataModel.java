fe948ac44d99afff88b2fea2664d4ef81302ac40
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-01-31 21:34:50 -0300

    feat(password-service): add property bucketSecret on BucketNewDataModel

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/buckets/models/BucketNewDataModel.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/buckets/models/BucketNewDataModel.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 public class BucketNewDataModel {
     private String bucketName;
     private String bucketDescription;
+    private String bucketSecret;
     private String userOwner;
 
     @Override
@@ -18,11 +19,11 @@ public class BucketNewDataModel {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         BucketNewDataModel that = (BucketNewDataModel) o;
-        return Objects.equals(bucketName, that.bucketName) && Objects.equals(bucketDescription, that.bucketDescription) && Objects.equals(userOwner, that.userOwner);
+        return Objects.equals(bucketName, that.bucketName) && Objects.equals(bucketDescription, that.bucketDescription) && Objects.equals(bucketSecret, that.bucketSecret) && Objects.equals(userOwner, that.userOwner);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(bucketName, bucketDescription, userOwner);
+        return Objects.hash(bucketName, bucketDescription, bucketSecret, userOwner);
     }
 }
