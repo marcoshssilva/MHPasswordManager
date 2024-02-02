6d07e02af01f81496d08883d2109af9f868f22c8
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-01 23:53:17 -0300

    feat(password-service): refactor - removed unused import

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/common/IResultDataFactory.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/common/IResultDataFactory.java
@@ -1,7 +1,5 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.common;
 
-import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.common.impl.ResultDataFactoryImpl;
-
 public interface IResultDataFactory<T> {
     IResultData<T> success(T data, String message);
     IResultData<T> error(String message);
