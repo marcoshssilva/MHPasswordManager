8aec7c81cf7c0a8a501711e92cf86661bb957813
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-04 00:51:35 -0300

    feat(password-service): refactor - remove unused constructors

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/keys/exceptions/KeyEncodedErrorConverterException.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/domain/services/data/keys/exceptions/KeyEncodedErrorConverterException.java
@@ -1,9 +1,6 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.services.data.keys.exceptions;
 
 public class KeyEncodedErrorConverterException extends Exception {
-    public KeyEncodedErrorConverterException() {
-        super();
-    }
 
     public KeyEncodedErrorConverterException(String message) {
         super(message);
@@ -11,13 +8,5 @@ public class KeyEncodedErrorConverterException extends Exception {
 
     public KeyEncodedErrorConverterException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public KeyEncodedErrorConverterException(Throwable cause) {
-        super(cause);
-    }
-
-    protected KeyEncodedErrorConverterException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
