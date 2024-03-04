105e56834372174b14d262196777da9ca9a5a3e1
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-03-03 22:07:21 -0300

    ci: Create script to fork project to private git

---
+++ fork-project.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+folders=("mhpasswordmanager-api-gateway" "mhpasswordmanager-config-services" "mhpasswordmanager-email-service" "mhpasswordmanager-file-service" "mhpasswordmanager-oauth2-server" "mhpasswordmanager-password-service" "mhpasswordmanager-service-registry" "mhpasswordmanager-user-service" "tool-mail" "tool-mongo" "tool-postgres" "tool-rabbitmq" "tool-redis")
+
+for folder in "${folders[@]}"; do
+    cd "$folder" || continue
+    git init
+    git branch -M main
+    git add .
+    git commit -m "update project - $3"
+    git remote add origin "https://$1:$2@app-gi.marcoshssilva.com.br/git/MHPasswordManager/$folder.git"
+    git fetch origin
+    git pull origin main
+    git push -u origin main
+    cd ..
+done
