c6ba52bbae17c803fda7be77c532b80e2eebc58e
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-04-25 05:22:15 -0300

    feat: Create methods for JwkUtils

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/utils/JwksUtils.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/utils/JwksUtils.java
@@ -1,12 +1,22 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2server.utils;
 
+import br.com.marcoshssilva.mhpasswordmanager.oauth2server.domain.exceptions.JwkLoaderFailException;
 import com.nimbusds.jose.jwk.RSAKey;
+import lombok.extern.slf4j.Slf4j;
 
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.UUID;
 
+@Slf4j
 public final class JwksUtils {
     private JwksUtils() {}
 
@@ -21,4 +31,26 @@ public final class JwksUtils {
                 .build();
     }
 
+
+    public static String getBase64X509PublicKeyFromBytes(byte[] bytes, String algorithm) {
+        String content = new String(bytes, StandardCharsets.UTF_8).replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replaceAll("\\s+", "");
+        try {
+            KeyFactory kf = KeyFactory.getInstance(algorithm);
+            PublicKey publicKey = kf.generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(content)));
+            return new String(Base64.getEncoder().encode(publicKey.getEncoded()), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new JwkLoaderFailException("Invalid public key", e);
+        }
+    }
+    
+    public static String getBase64PKCS8PrivateKeyFromBytes(byte[] bytes, String algorithm) {
+        String content = new String(bytes, StandardCharsets.UTF_8).replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "").replaceAll("\\s+", "");
+        try {
+            KeyFactory kf = KeyFactory.getInstance(algorithm);
+            PrivateKey privateKey = kf.generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(content)));
+            return new String(Base64.getEncoder().encode(privateKey.getEncoded()), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new JwkLoaderFailException("Invalid private key", e);
+        }
+    }
 }
