3fa8958d94dc537d8bb196ce3a1385cdd3d4fb7e
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-05-01 03:38:38 -0300

    feat: Created BusinessRuleException class

---
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/domain/exceptions/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package br.com.marcoshssilva.mhpasswordmanager.oauth2server.domain.exceptions;
+
+public class BusinessRuleException extends Exception {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
