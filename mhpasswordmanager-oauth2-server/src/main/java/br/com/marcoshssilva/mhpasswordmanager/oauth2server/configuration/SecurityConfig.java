22417459bbe614ce990728f2324aae8d1c1f4075
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-05-16 23:34:50 -0300

    lint: Remove unused import

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/configuration/SecurityConfig.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/configuration/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @lombok.RequiredArgsConstructor
 @EnableWebSecurity
