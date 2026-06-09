25474edcf851b194d9b4861721620453717ef470
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-06-09 00:24:38 -0300

    Update MhPasswordManagerApiGatewayApplicationTests logger naming and mock logic

--- mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/apigateway/MhPasswordManagerApiGatewayApplicationTests.java
+++ mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/apigateway/MhPasswordManagerApiGatewayApplicationTests.java
@@ -3,25 +3,23 @@ package br.com.marcoshssilva.mhpasswordmanager.apigateway;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ConfigurableApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class MhPasswordManagerApiGatewayApplicationTests {
-	private final Logger LOG = LoggerFactory.getLogger(MhPasswordManagerApiGatewayApplicationTests.class);
+	private final Logger log = LoggerFactory.getLogger(MhPasswordManagerApiGatewayApplicationTests.class);
 
 	@DisplayName("Should initialize project with success")
 	@Test
 	void contextLoads() {
 		assertDoesNotThrow(() -> {
-			LOG.info("Project started with success!");
+			log.info("Project started with success!");
 		});
 	}
 
@@ -33,7 +31,7 @@ class MhPasswordManagerApiGatewayApplicationTests {
 			mocked.when(() -> {
 						SpringApplication.run(MhPasswordManagerApiGatewayApplication.class, args);
 					})
-					.thenReturn(Mockito.mock(ConfigurableApplicationContext.class));
+					.thenReturn(null);
 
 			MhPasswordManagerApiGatewayApplication.main(args);
 			mocked.verify(() -> {
