9ef04597bd1cb90f4ab93f1ab822fb897fc8acb8
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-06-24 01:56:51 -0300

    feat: Disable session and make API stateless by default

--- mhpasswordmanager-service-registry/src/main/java/br/com/marcoshssilva/mhpasswordmanager/serviceregistry/config/BasicAuthenticationSecurity.java
+++ mhpasswordmanager-service-registry/src/main/java/br/com/marcoshssilva/mhpasswordmanager/serviceregistry/config/BasicAuthenticationSecurity.java
@@ -5,24 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class BasicAuthenticationSecurity {
-    protected final String[] endpointsIgnoreCsrfAttack = new String[] { "/eureka", "/eureka/**" };
-    protected final String[] endpointsIgnoreAuthentication = new String[] { "/actuator/**" };
+    protected final String[] endpointsIgnoreAuthentication = new String[] { "/actuator/health/**" };
     protected final String[] endpointsNeedAuthentication = new String[] { "/eureka/**" };
 
     @Bean
     SecurityFilterChain basicAuthenticationSecurityFilterChainConfigurer(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.ignoringRequestMatchers(endpointsIgnoreCsrfAttack));
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.httpBasic();
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(endpointsIgnoreAuthentication).permitAll()
                 .requestMatchers(endpointsNeedAuthentication).authenticated()
                 .anyRequest().authenticated());
-        http.httpBasic();
+
         return http.build();
     }
 }
