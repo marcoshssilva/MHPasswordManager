a2f6e1365b86ca2ada0c924cd57ab9479c2e9503
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-05 01:11:07 -0300

    feat: Implements LOG to trace http errors

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/web/errors/ControllerAdviceResolver.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/web/errors/ControllerAdviceResolver.java
@@ -5,6 +5,8 @@ import br.com.marcoshssilva.mhpasswordmanager.oauth2server.domain.exceptions.Bus
 import br.com.marcoshssilva.mhpasswordmanager.oauth2server.domain.exceptions.FailSendEmailException;
 import br.com.marcoshssilva.mhpasswordmanager.oauth2server.web.data.responses.HttpJsonResponse;
 import br.com.marcoshssilva.mhpasswordmanager.oauth2server.domain.exceptions.CannotRegisterUserException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,9 +18,11 @@ import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class ControllerAdviceResolver {
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerAdviceResolver.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<HttpJsonResponse<Object>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception, HttpServletRequest req, HttpServletResponse res) {
+        LOG.error("MethodArgumentNotValidException: {}", exception.getMessage(), exception);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(HttpJsonResponse.builder()
                         .status(StatusTypeEnum.ERROR)
@@ -29,6 +33,7 @@ public class ControllerAdviceResolver {
 
     @ExceptionHandler(CannotRegisterUserException.class)
     public ResponseEntity<HttpJsonResponse<Object>> cannotRegisterUserExceptionHandler(CannotRegisterUserException exception, HttpServletRequest req, HttpServletResponse res) {
+        LOG.error("CannotRegisterUserException: {}", exception.getMessage(), exception);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(HttpJsonResponse.builder()
                         .status(StatusTypeEnum.ERROR)
@@ -39,6 +44,7 @@ public class ControllerAdviceResolver {
 
     @ExceptionHandler(FailSendEmailException.class)
     public ResponseEntity<HttpJsonResponse<Object>> failSendEmailExceptionHandler(FailSendEmailException exception, HttpServletRequest req, HttpServletResponse res) {
+        LOG.error("FailSendEmailException: {}", exception.getMessage(), exception);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(HttpJsonResponse.builder()
                         .status(StatusTypeEnum.ERROR)
@@ -49,6 +55,7 @@ public class ControllerAdviceResolver {
 
     @ExceptionHandler(BusinessRuleException.class)
     public ResponseEntity<HttpJsonResponse<Void>> businessRuleExceptionHandler(BusinessRuleException exception, HttpServletRequest req, HttpServletResponse res) {
+        LOG.error("BusinessRuleException: {}", exception.getMessage(), exception);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(HttpJsonResponse.<Void>builder()
                         .status(StatusTypeEnum.ERROR)
