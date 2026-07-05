2c917ac25e8ef5e43add92a624279758e3679aa6
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-05 00:30:12 -0300

    feat: Refactor FailSendEmailException to remove unused constructor

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/domain/exceptions/FailSendEmailException.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/domain/exceptions/FailSendEmailException.java
@@ -1,10 +1,6 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2server.domain.exceptions;
 
 public class FailSendEmailException extends Exception {
-    public FailSendEmailException(String message) {
-        super(message);
-    }
-
     public FailSendEmailException(String message, Throwable cause) {
         super(message, cause);
     }
