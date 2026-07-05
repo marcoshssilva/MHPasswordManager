883ee1fa876f03046aa0b661fc33d3e337c3535d
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-05 01:27:05 -0300

    feat: Add overloaded constructor to BusinessRuleException with cause support

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/domain/exceptions/BusinessRuleException.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/domain/exceptions/BusinessRuleException.java
@@ -4,4 +4,8 @@ public class BusinessRuleException extends Exception {
     public BusinessRuleException(String message) {
         super(message);
     }
+
+    public BusinessRuleException(String message, Exception e) {
+        super(message, e);
+    }
 }
