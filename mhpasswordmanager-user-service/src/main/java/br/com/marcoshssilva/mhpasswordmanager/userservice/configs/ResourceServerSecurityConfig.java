b21c048c1b0b97b69bfaa8bf900a2d9f75a3f504
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-10 20:23:43 -0300

    refactor: Remove Redis session support and configure stateless session management

--- mhpasswordmanager-user-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/userservice/configs/ResourceServerSecurityConfig.java
+++ mhpasswordmanager-user-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/userservice/configs/ResourceServerSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -33,6 +34,7 @@ public class ResourceServerSecurityConfig {
         http.oauth2ResourceServer()
                 .jwt()
                 .jwtAuthenticationConverter(jwtAuthenticationConverter());
+        http.sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
