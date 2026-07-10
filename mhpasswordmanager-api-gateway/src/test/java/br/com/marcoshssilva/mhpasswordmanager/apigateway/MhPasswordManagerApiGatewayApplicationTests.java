601070785e0221255f3ad648b55c1f4247f1c9f3
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-10 20:54:35 -0300

    refactor: Apply `@ActiveProfiles("test")`, simplify test assertions, and move test-specific properties to `application-test.yml`

--- mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/apigateway/MhPasswordManagerApiGatewayApplicationTests.java
+++ mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/apigateway/MhPasswordManagerApiGatewayApplicationTests.java
@@ -5,31 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(properties = {
-		"spring.cloud.config.enabled=false",
-		"spring.config.import=",
-		"eureka.client.enabled=false",
-		"eureka.client.register-with-eureka=false",
-		"eureka.client.fetch-registry=false"
-})
+@ActiveProfiles({"test"})
+@SpringBootTest(classes = MhPasswordManagerApiGatewayApplication.class)
 class MhPasswordManagerApiGatewayApplicationTests {
 	private final Logger log = LoggerFactory.getLogger(MhPasswordManagerApiGatewayApplicationTests.class);
 
 	@DisplayName("Should initialize project with success")
 	@Test
 	void contextLoads() {
-		assertDoesNotThrow(() -> {
-			log.info("Project started with success!");
-		});
-	}
-
-	@DisplayName("Should call main but using a ")
-	@Test
-	void shouldExposeApplicationClass() {
-		assertNotNull(MhPasswordManagerApiGatewayApplication.class);
+		assertDoesNotThrow(() -> log.info("Project started with success!"));
 	}
 }
