4f66e5c33d251669be5c972ed0136e105f3da98a
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-10 21:13:23 -0300

    refactor: Apply `@ActiveProfiles("test")`, simplify test assertions, and move test-specific properties to `application-test.yml`

--- mhpasswordmanager-email-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/emailservice/ApplicationTests.java
+++ mhpasswordmanager-email-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/emailservice/ApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 @EnableAutoConfiguration(exclude = {RabbitAutoConfiguration.class})
 class ApplicationTests {
 
