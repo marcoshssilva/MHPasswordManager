da18a13aa6965cd74c990cca2db91ac255fe260a
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-05 12:51:13 -0300

    feat(config): update ObjectMapper bean with Jackson2ObjectMapperBuilder integration

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/configs/ObjectMapperConfig.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/configs/ObjectMapperConfig.java
@@ -1,13 +1,14 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ObjectMapperConfig {
     @Bean
-    ObjectMapper objectMapper() {
-        return new ObjectMapper();
+    ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.createXmlMapper(false).build();
     }
 }
