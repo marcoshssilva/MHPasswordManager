e329f7a3018d409db409bee6a3063ad5a140d01f
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-05 12:52:26 -0300

    feat(domain): add FileProcessingStatus enum to represent file processing states

---
+++ mhpasswordmanager-file-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/fileservice/domain/enums/FileProcessingStatus.java
@@ -0,0 +1,5 @@
+package br.com.marcoshssilva.mhpasswordmanager.fileservice.domain.enums;
+
+public enum FileProcessingStatus {
+    UPLOAD_RECEIVED, SOURCE_STORED, ENCRYPTING, FINALIZING, READY, FAILED
+}
