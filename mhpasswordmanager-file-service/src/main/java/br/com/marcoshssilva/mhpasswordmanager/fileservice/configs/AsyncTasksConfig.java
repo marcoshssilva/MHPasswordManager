d763f7abad0d8bcb84c9e428c2bc0d51f2597b09
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-05 12:51:36 -0300

    feat(config): add AsyncTasksConfig with @EnableAsync configuration

---
+++ mhpasswordmanager-file-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/fileservice/configs/AsyncTasksConfig.java
@@ -0,0 +1,8 @@
+package br.com.marcoshssilva.mhpasswordmanager.fileservice.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncTasksConfig { }
