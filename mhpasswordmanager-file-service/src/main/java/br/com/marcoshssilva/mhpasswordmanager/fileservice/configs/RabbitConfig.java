b5e5578661a35ec0bde32ecc3862eea2fd8c9c6e
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-05 12:51:29 -0300

    feat(rabbitmq): add RabbitMQ configuration with Jackson-based message conversion

---
+++ mhpasswordmanager-file-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/fileservice/configs/RabbitConfig.java
@@ -0,0 +1,30 @@
+package br.com.marcoshssilva.mhpasswordmanager.fileservice.configs;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public MessageConverter converter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.INFERRED);
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory, MessageConverter converter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter);
+        return rabbitTemplate;
+    }
+}
