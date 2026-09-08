c74490320b66949ceff15107efe3f9e3e8eb4844
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-08 10:54:12 -0300

    test: add MongoMockTestServerConfiguration for unit testing with in-memory MongoDB

---
+++ mhpasswordmanager-file-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/fileservice/MongoMockTestServerConfiguration.java
@@ -0,0 +1,34 @@
+package br.com.marcoshssilva.mhpasswordmanager.fileservice;
+
+import br.com.marcoshssilva.mhpasswordmanager.fileservice.domain.repositories.StoredFileKeyRepository;
+
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@TestConfiguration
+@EnableMongoRepositories(basePackageClasses = StoredFileKeyRepository.class)
+public class MongoMockTestServerConfiguration {
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory) {
+        return new MongoTemplate(mongoDbFactory);
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDbFactory(MongoServer mongoServer) {
+        String connectionString = mongoServer.getConnectionString();
+        return new SimpleMongoClientDatabaseFactory(connectionString + "/test");
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public MongoServer mongoServer() {
+        MongoServer mongoServer = new MongoServer(new MemoryBackend());
+        mongoServer.bind();
+        return mongoServer;
+    }
+}
