17a9844e2da23cc2b271e16cae3b39775c37b4ef
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-08 10:30:43 -0300

    test: add logging and assertion to `contextLoads` in ApplicationTests

--- mhpasswordmanager-file-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/fileservice/ApplicationTests.java
+++ mhpasswordmanager-file-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/fileservice/ApplicationTests.java
@@ -1,14 +1,21 @@
 package br.com.marcoshssilva.mhpasswordmanager.fileservice;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = Application.class)
 class ApplicationTests {
+	private final Logger LOG = LoggerFactory.getLogger(ApplicationTests.class);
 
 	@Test
-	void contextLoads() {}
+	void contextLoads() {
+		assertDoesNotThrow(() -> LOG.info("Project started with success!"));
+	}
 
 }
