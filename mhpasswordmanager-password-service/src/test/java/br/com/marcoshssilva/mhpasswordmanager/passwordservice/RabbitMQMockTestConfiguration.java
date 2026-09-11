e26c450be2e344b8b3b0748e5069d70848565f16
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-10 23:33:51 -0300

    test: add mock RabbitMQ test configuration
    - Introduced `RabbitMQMockTestConfiguration` to enable mock RabbitMQ setup for unit tests.
    - Added a mocked `ConnectionFactory` bean using `MockConnectionFactoryFactory` for simulating RabbitMQ interactions in a test environment.

---
+++ mhpasswordmanager-password-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/RabbitMQMockTestConfiguration.java
@@ -0,0 +1,16 @@
+package br.com.marcoshssilva.mhpasswordmanager.passwordservice;
+
+import com.github.fridujo.rabbitmq.mock.compatibility.MockConnectionFactoryFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class RabbitMQMockTestConfiguration {
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory(MockConnectionFactoryFactory.build());
+    }
+}
