a27ed596bbb9f0e763f4b8d1087345839c90846a
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-10 23:58:27 -0300

    feat: add constructor overload to `FailSendEmailException`
    - Added a new constructor accepting only a message parameter to improve exception usability.

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/domain/exceptions/FailSendEmailException.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/domain/exceptions/FailSendEmailException.java
@@ -1,6 +1,10 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2server.domain.exceptions;
 
 public class FailSendEmailException extends Exception {
+    public FailSendEmailException(String message) {
+        super(message);
+    }
+
     public FailSendEmailException(String message, Throwable cause) {
         super(message, cause);
     }
