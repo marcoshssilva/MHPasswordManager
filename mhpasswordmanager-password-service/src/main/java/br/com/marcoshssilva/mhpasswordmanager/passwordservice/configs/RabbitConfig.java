6d723bebd8fcd8e784a2b11b496dc4117fe68a49
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-10 23:31:13 -0300

    feat: configure RabbitMQ queues, exchange, and bindings for file encryption events

--- mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/configs/RabbitConfig.java
+++ mhpasswordmanager-password-service/src/main/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/configs/RabbitConfig.java
@@ -1,6 +1,11 @@
 package br.com.marcoshssilva.mhpasswordmanager.passwordservice.configs;
 
+import br.com.marcoshssilva.mhpasswordmanager.passwordservice.amqp.queues.FileEncryptionWorkerQueue;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
@@ -12,6 +17,41 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class RabbitConfig {
+
+    @Bean
+    public Queue encryptionCompletedQueue() {
+        return new Queue(FileEncryptionWorkerQueue.ENCRYPTION_COMPLETED, true);
+    }
+
+    @Bean
+    public Queue encryptionFailedQueue() {
+        return new Queue(FileEncryptionWorkerQueue.ENCRYPTION_FAILED, true);
+    }
+
+    @Bean
+    public Queue encryptionRequestedQueue() {
+        return new Queue(FileEncryptionWorkerQueue.ENCRYPTION_REQUESTED, true);
+    }
+
+    @Bean
+    public TopicExchange filesEventsExchange() {
+        return new TopicExchange(FileEncryptionWorkerQueue.EXCHANGE);
+    }
+
+    @Bean
+    public Binding encryptionCompletedBinding(Queue encryptionCompletedQueue, TopicExchange filesEventsExchange) {
+        return BindingBuilder.bind(encryptionCompletedQueue).to(filesEventsExchange).with(FileEncryptionWorkerQueue.ENCRYPTION_COMPLETED);
+    }
+
+    @Bean
+    public Binding encryptionFailedBinding(Queue encryptionFailedQueue, TopicExchange filesEventsExchange) {
+        return BindingBuilder.bind(encryptionFailedQueue).to(filesEventsExchange).with(FileEncryptionWorkerQueue.ENCRYPTION_FAILED);
+    }
+
+    @Bean
+    public Binding encryptionRequestedBinding(Queue encryptionRequestedQueue, TopicExchange filesEventsExchange) {
+        return BindingBuilder.bind(encryptionRequestedQueue).to(filesEventsExchange).with(FileEncryptionWorkerQueue.ENCRYPTION_REQUESTED);
+    }
 
     @Bean
     public MessageConverter converter() {
