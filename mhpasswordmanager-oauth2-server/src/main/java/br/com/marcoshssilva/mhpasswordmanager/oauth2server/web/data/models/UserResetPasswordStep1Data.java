6dc81b32187fc19cc52d1ec0e67152f00ab9112b
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-10 23:59:04 -0300

    feat: add `@Builder` and `@AllArgsConstructor` to `UserResetPasswordStep1Data` and `UserResetPasswordStep2Data`
    - Introduced `@Builder` annotation to facilitate flexible object creation.
    - Added `@AllArgsConstructor` for streamlined instantiation of both data models.

--- mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/web/data/models/UserResetPasswordStep1Data.java
+++ mhpasswordmanager-oauth2-server/src/main/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/web/data/models/UserResetPasswordStep1Data.java
@@ -1,5 +1,7 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2server.web.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +10,8 @@ import lombok.ToString;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Getter
