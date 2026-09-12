cf8b5c1bc93f27a0ed2bb8c1bc98800b570a9c29
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-12 19:10:43 -0300

    test: update `ApplicationTests` with `shouldCallSpringApplicationRun` verification
    - Refactored test to use mocked static import for verifying `SpringApplication.run` invocation in the main method.

--- mhpasswordmanager-password-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/MhPasswordManagerPasswordServiceApplicationTests.java
+++ mhpasswordmanager-password-service/src/test/java/br/com/marcoshssilva/mhpasswordmanager/passwordservice/MhPasswordManagerPasswordServiceApplicationTests.java
@@ -5,15 +5,18 @@ import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.enums.Passw
 import br.com.marcoshssilva.mhpasswordmanager.passwordservice.domain.repositories.UserPasswordKeyTypeRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -23,7 +26,6 @@ class MhPasswordManagerPasswordServiceApplicationTests {
     @Autowired
     private UserPasswordKeyTypeRepository userPasswordKeyTypeRepository;
 
-
     @DisplayName("Should initialize project with success")
     @Test
     void contextLoads() {
@@ -32,10 +34,18 @@ class MhPasswordManagerPasswordServiceApplicationTests {
         });
     }
 
-    @DisplayName("Should call main but using a ")
+    @DisplayName("Should call SpringApplication.run when main is executed")
     @Test
-    void shouldExposeApplicationClass() {
-        assertNotNull(MhPasswordManagerPasswordServiceApplication.class);
+    void shouldCallSpringApplicationRun() {
+        try (MockedStatic<SpringApplication> mocked = mockStatic(SpringApplication.class)) {
+            String[] args = new String[]{};
+            mocked.when(() -> SpringApplication.run(MhPasswordManagerPasswordServiceApplication.class, args))
+                    .thenReturn(null);
+
+            MhPasswordManagerPasswordServiceApplication.main(args);
+
+            mocked.verify(() -> SpringApplication.run(MhPasswordManagerPasswordServiceApplication.class, args));
+        }
     }
 
     @DisplayName("Register all UserPasswordKeys in Embedded Database")
