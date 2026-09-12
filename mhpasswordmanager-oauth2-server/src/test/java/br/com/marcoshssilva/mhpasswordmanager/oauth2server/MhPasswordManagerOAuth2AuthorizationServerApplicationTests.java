e98a14bc6cc9cebaf854f93e1839a999810d3700
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-12 19:10:34 -0300

    test: refactor `ApplicationTests` with `shouldCallSpringApplicationRun` test
    - Replaced `contextLoads` with `shouldCallSpringApplicationRun` test to verify `SpringApplication.run` invocation in the main method using mocked static import.

--- mhpasswordmanager-oauth2-server/src/test/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/MhPasswordManagerOAuth2AuthorizationServerApplicationTests.java
+++ mhpasswordmanager-oauth2-server/src/test/java/br/com/marcoshssilva/mhpasswordmanager/oauth2server/MhPasswordManagerOAuth2AuthorizationServerApplicationTests.java
@@ -1,20 +1,25 @@
 package br.com.marcoshssilva.mhpasswordmanager.oauth2server;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootTest(classes = MhPasswordManagerOAuth2AuthorizationServerApplication.class)
-@ActiveProfiles({"test"})
+import static org.mockito.Mockito.mockStatic;
+
 class MhPasswordManagerOAuth2AuthorizationServerApplicationTests {
 
-	private final Logger log = LoggerFactory.getLogger(MhPasswordManagerOAuth2AuthorizationServerApplicationTests.class);
-
+	@DisplayName("Should call SpringApplication.run when main is executed")
 	@Test
-	void contextLoads() {
-		log.info("Application Started");
-	}
+	void shouldCallSpringApplicationRun() {
+		try (MockedStatic<SpringApplication> mocked = mockStatic(SpringApplication.class)) {
+			String[] args = new String[]{};
+			mocked.when(() -> SpringApplication.run(MhPasswordManagerOAuth2AuthorizationServerApplication.class, args))
+					.thenReturn(null);
 
+			MhPasswordManagerOAuth2AuthorizationServerApplication.main(args);
+
+			mocked.verify(() -> SpringApplication.run(MhPasswordManagerOAuth2AuthorizationServerApplication.class, args));
+		}
+	}
 }
