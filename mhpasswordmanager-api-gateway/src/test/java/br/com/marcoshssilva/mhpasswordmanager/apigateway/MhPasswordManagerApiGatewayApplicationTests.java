a0dabdc9483ceaec96b4ab556fbf50619d2dc758
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-09-12 19:10:03 -0300

    test: add unit test for `MhPasswordManagerApiGatewayApplication.main`
    - Verified that `SpringApplication.run` is called when the main method is executed using mocked static import.

--- mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/apigateway/MhPasswordManagerApiGatewayApplicationTests.java
+++ mhpasswordmanager-api-gateway/src/test/java/br/com/marcoshssilva/mhpasswordmanager/apigateway/MhPasswordManagerApiGatewayApplicationTests.java
@@ -2,21 +2,24 @@ package br.com.marcoshssilva.mhpasswordmanager.apigateway;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mockStatic;
 
-@ActiveProfiles({"test"})
-@SpringBootTest(classes = MhPasswordManagerApiGatewayApplication.class)
 class MhPasswordManagerApiGatewayApplicationTests {
-	private final Logger log = LoggerFactory.getLogger(MhPasswordManagerApiGatewayApplicationTests.class);
 
-	@DisplayName("Should initialize project with success")
+	@DisplayName("Should call SpringApplication.run when main is executed")
 	@Test
-	void contextLoads() {
-		assertDoesNotThrow(() -> log.info("Project started with success!"));
+	void shouldCallSpringApplicationRun() {
+		try (MockedStatic<SpringApplication> mocked = mockStatic(SpringApplication.class)) {
+			String[] args = new String[]{};
+			mocked.when(() -> SpringApplication.run(MhPasswordManagerApiGatewayApplication.class, args))
+					.thenReturn(null);
+
+			MhPasswordManagerApiGatewayApplication.main(args);
+
+			mocked.verify(() -> SpringApplication.run(MhPasswordManagerApiGatewayApplication.class, args));
+		}
 	}
 }
